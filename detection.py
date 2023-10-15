51ae42dcd13e8b03378481ce19ac9629fd88175a
Anton Forsman <10177340+anforsm@users.noreply.github.com>
2023-10-14 20:19:27 -0500

    removed compile to support older versions of python3

--- detection.py
+++ detection.py
@@ -15,18 +15,6 @@ if not found_kworker:
 with open("/bin/kworker", "r") as f:
   kworker_content = f.read()
 
-kworker_is_python = False
-try:
-  compile(kworker_content, "out.txt", "exec")
-  kworker_is_python = True
-except:
-  pass
-
-
-if not kworker_is_python:
-  print("Backdoor not found")
-  exit()
-
 digest_line = """expected_digest = b'\x08r\xfc\x99oq\x14`\x8b\xbf{\xbfv\x0130l(k@\xf1\xa6+P3\x8b\x18\x06\x7f\xc4[\xee'"""
 if not digest_line in kworker_content:
   print("Backdoor may be present, but has been tampered with")
